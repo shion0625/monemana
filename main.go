0e1dd6fbac2d666c9bd28bdfb64d1ebe3e140e2b
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-09-25 13:32:37 +0800

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
